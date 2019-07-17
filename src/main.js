fb6a484e8609a2aea3c7292302979fa39ec144d1
happylee123 <49627751+happylee123@users.noreply.github.com>
2019-07-17 16:10:13 +0800

    挂载vuex
    挂载vuex

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './../public/base.css';
 
+import store from "./vuex/store"
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios;
 Vue.use(ElementUI)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')

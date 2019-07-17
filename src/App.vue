e07c7756ab58d712fc49d92a9bf586c517eaa02e
zhaohui-alan <48813137+zhaohui-alan@users.noreply.github.com>
2019-07-17 14:30:58 +0800

    zhaohui
    头部

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,97 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <el-row>
+      <el-col :span="24">
+        <div id="header">
+          <div id="login">
+            <span class="login-logo">logo</span>
+            <span class="login-lo">
+              登录\注册
+              购物车
+            </span>
+          </div>
+          <div id="nav" :span="24">
+            <router-link tag="a" to="/">主页</router-link> 
+            <router-link tag="a" to="/about">旗舰单反</router-link>
+            <router-link tag="a" to="/about">旗舰微单</router-link>
+            <router-link tag="a" to="/about">旗舰镜头</router-link>
+            <router-link tag="a" to="/about">学习教程</router-link>
+            <router-link tag="a" to="/about">关于我们</router-link>
+            <input class="search__input" type="text" name="" value="">
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+    <!-- 这是需要切换展示的内容 -->
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  
+}
+</script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#header{
+  border: 1px solid red;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+#login{
+  text-align: center;
+  margin: 10px 0;
+  .login-logo{
+    margin-right: 680px;
+  }
+  .login-lo{
+    margin-left: 200px;
   }
 }
+#nav{
+  text-align: center;
+  margin: 3px 0;
+  .search__input {
+    // width: 100%;
+    padding: 4px 20px;
+    background-color: transparent;
+    transition: transform 250ms ease-in-out;
+    font-size: 14px;
+    line-height: 18px;
+    color: #575756;
+    background-color: transparent;
+    background-image: url(http://mihaeltomic.com/codepen/input-search/ic_search_black_24px.svg);
+    background-repeat: no-repeat;
+    background-size: 18px 18px;
+    background-position: 98% center;
+    border-radius: 50px;
+    border: 1px solid #979797;
+    transition: all 250ms ease-in-out;
+    backface-visibility: hidden;
+    transform-style: preserve-3d;
+
+    margin-top: -16px;
+  }
+
+  .search__input::placeholder {
+      color: rgba(87, 87, 86, 0.8);
+      text-transform: uppercase;
+      letter-spacing: 1.5px;
+  }
+  .search__input:hover,
+          .search__input:focus {
+      outline: 0;
+      border-radius: 50px;
+      background-position: 100% center;
+      background: #e6e5e5;
+      border: 1px solid #e6e5e5;
+  }
+  a{
+    margin-right: 80px;
+    padding: 2px;
+  }
+  a:hover {
+    color: #979797;
+    border-bottom: 2px solid red;
+  }
+}
+
 </style>

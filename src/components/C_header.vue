b60237496a9d6679ad3c9421e2da18032d3833ee
happylee123 <49627751+happylee123@users.noreply.github.com>
2019-07-18 09:20:16 +0800

    111lee
    111lee

---
+++ src/components/C_header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h2>还有多久多久</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+ 
+}
+</script>
+
+<style scope lang="less">
+  
+</style>
---
+++ src/views/C_Backstage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Backstage">
+     <Cheader></Cheader>
+  </div>
+</template>
+
+<script>
+import Cheader from "../components/C_header"
+export default {
+  components:{
+      Cheader
+  }
+}
+</script>
